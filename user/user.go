c00fe1f094db60143fd1b1373f728b56e6854934
Prosper Onogberie <prosper@ip-192-168-8-100.ec2.internal>
2020-11-19 11:58:59 +0100

    fix temporal for typo in method name in proto

--- user/user.go
+++ user/user.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net"
 
-	bt "github.com/Prosp3r/buytrance/user/pb"
+	btuser "github.com/Prosp3r/buytrance/user/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -26,49 +26,49 @@ func main() {
 	fmt.Printf("OriCalc Server started on port %v \n", port)
 
 	s := grpc.NewServer()
-	bt.RegisterUSERServiceServer(s, &user{})
+	btuser.RegisterUSERServiceServer(s, &user{})
 
-	//ori.RegisterORIServiceServer(s, &server{})
+	//bt.RegisterORIServiceServer(s, &server{})
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed serving ORI with error : %v \n", err)
+		log.Fatalf("Failed serving User with error : %v \n", err)
 	}
 }
 
 //AddUser - Method will add a new user to the system
-func (c *user) AddUser(ctx context.Context, r *bt.User) (*bt.UserResponse, error) {
+func (c *user) AddUser(ctx context.Context, r *btuser.User) (*btuser.UserResponse, error) {
 
-	var response *bt.UserResponse
+	var response *btuser.UserResponse
 	return response, nil
 }
 
 //GetUser - will return user's detailed information
-func (c *user) GetUser(ctx context.Context, r *bt.IDString) (*bt.UserResponse, error) {
-	var response *bt.UserResponse
+func (c *user) GetUser(ctx context.Context, r *btuser.IDString) (*btuser.UserResponse, error) {
+	var response *btuser.UserResponse
 	return response, nil
 }
 
 //GetUsers -  will return a list of users
-func (c *user) GetUsers(ctx context.Context, r *bt.Emptyentry) (*bt.Users, error) {
-	var response *bt.Users
+func (c *user) GetUsers(ctx context.Context, r *btuser.Emptyentry) (*btuser.Users, error) {
+	var response *btuser.Users
 	return response, nil
 }
 
 //UpdateUser - will update a user's information
-func (c *user) UpdateUser(ctx context.Context, r *bt.User) (*bt.UserResponse, error) {
-	var response *bt.UserResponse
+func (c *user) UpdateUser(ctx context.Context, r *btuser.User) (*btuser.UserResponse, error) {
+	var response *btuser.UserResponse
 	return response, nil
 }
 
 //DeactivateUser - Will flag a user's profile/account deactivated
-func (c *user) DeactivateUser(ctx context.Context, r *bt.User) (*bt.BOOLValue, error) {
-	var response *bt.BOOLValue
+func (c *user) DeactivateUser(ctx context.Context, r *btuser.User) (*btuser.BOOLValue, error) {
+	var response *btuser.BOOLValue
 	return response, nil
 }
 
 //UpdateUser - will update a user's information
 //This is a temporary fix for cache issues resulting from typo in method name UpdateUser in proto file
-func (c *user) UdateUser(ctx context.Context, r *bt.User) (*bt.UserResponse, error) {
-	var response *bt.UserResponse
+func (c *user) UdateUser(ctx context.Context, r *btuser.User) (*btuser.UserResponse, error) {
+	var response *btuser.UserResponse
 	return response, nil
 }
