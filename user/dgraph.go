d1cb04e694057cdeba2ada1010ef96da9962a96e
Prosper Onogberie <prosper@ip-192-168-8-100.ec2.internal>
2020-11-26 00:47:50 +0100

    Moving all to single file

--- user/dgraph.go
+++ user/dgraph.go
@@ -12,17 +12,3 @@ import (
 	"google.golang.org/grpc"
 )
 
-var dgServer = "localhost:9080"
-
-//DgraphCONN - Opens connection to Dgraph database.
-func DgraphCONN() *dgo.Dgraph {
-	conn, err := grpc.Dial(dgServer, grpc.WithInsecure())
-	if err != nil {
-		log.Fatal("Could not connect to DGraph gRPC")
-	}
-	defer conn.Close()
-	dc := api.NewDgraphClient(conn)
-	dg := dgo.NewDgraphClient(dc)
-
-	return dg
-}
--- user/user.go
+++ user/user.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 
 	btuser "github.com/Prosp3r/buytrance/user/pb"
+	"github.com/dgraph-io/dgo/protos/api"
 	"github.com/gorilla/mux"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+
+	"github.com/dgraph-io/dgo/protos/api"
+	"github.com/dgraph-io/dgo/v200"
 )
+
+
+
+var dgServer = "localhost:9080"
+
+//DgraphCONN - Opens connection to Dgraph database.
+func DgraphCONN() *dgo.Dgraph {
+	conn, err := grpc.Dial(dgServer, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Could not connect to DGraph gRPC")
+	}
+	defer conn.Close()
+	dc := api.NewDgraphClient(conn)
+	dg := dgo.NewDgraphClient(dc)
+
+	return dg
+}
 
 /*/User type
 type Usertype struct {
@@ -93,14 +116,14 @@ func (c *user) AddUser(ctx context.Context, r *btuser.User) (*btuser.UserRespons
 			return nil, err
 		}
 		response.User = adduser
-		return response, nil
+		//return response, nil
 	}
 
 	//store in database if not
 	//If already exists, return user information and send alert code to email or phone number on file for login
 	//response.User = adduser //temporary assignment this will change
 
-	//return response, nil
+	return response, nil
 }
 
 //AddUser - for Handling JSON API Requests mapped to AddUser gRPC method
@@ -172,4 +195,150 @@ func DeactivateUser(w http.ResponseWriter, r *http.Request) {
 func (c *user) UdateUser(ctx context.Context, r *btuser.User) (*btuser.UserResponse, error) {
 	var response *btuser.UserResponse
 	return response, nil
+}
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//::::::::::::::::::::::USER UTILITY ::::::::::::::::::::::::::::::::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//UserSchemas -
+var UserSchemas = `username: string .
+email: string @index(exact) .
+accountstatus: string .
+verificationcode string .
+codestatus string .
+datejoined datetime .
+lastupdated datetime .
+
+type User struct {
+	username
+	email
+	accountstatus
+	verificationcode
+	codestatus
+	datejoin
+	lastupdated
+}`
+
+//AddUserToDGraph - adds a new user node to the DGraph database
+//Takes in a variable *btuser.User struct and returns *api.Response or error
+func AddUserToDGraph(userInfoJSON *btuser.User) (*btuser.User, error) {
+	dgraph := DgraphCONN()
+	op := &api.Operation{}
+	op.Schema = `
+	username: string .
+	email: string @index(exact) .
+	accountstatus: string .
+	verificationcode string .
+	codestatus string .
+	datejoined datetime .
+	lastupdated datetime .
+
+	type User {
+		username
+		email
+		accountstatus
+		verificationcode
+		codestatus
+		datejoin
+		lastupdated
+	}`
+	ctx := context.Background()
+	err := dgraph.Alter(ctx, op)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//datejoined := time.Now()
+	//dob := time.Date(1980, 01, 01, 23, 0, 0, 0, time.UTC)
+	mu := &api.Mutation{
+		CommitNow: true,
+	}
+	pb, err := json.Marshal(userInfoJSON)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mu.SetJson = pb
+	assigned, err := dgraph.NewTxn().Mutate(ctx, mu)
+	if err != nil {
+		log.Fatalf("Failed with error: \n %+v \n", err)
+	}
+
+	variables := map[string]string{"$email": assigned.Uids[userInfoJSON.Email]}
+	query := `query me($email: string){
+		me(func: email($email)){
+			uid
+			username
+			email
+			accountstatus
+			verificationcode
+			codestatus
+			datejoin
+			lastupdated
+		}
+	}`
+	resp, err := dgraph.NewTxn().QueryWithVars(ctx, query, variables)
+	if err != nil {
+		log.Fatalf("Failed with error: \n %+v \n", err)
+	}
+	var person *btuser.User
+	err = json.Unmarshal(resp.Json, &person)
+	if err != nil {
+		log.Fatalf("Failed with error : \n %v \n", err)
+	}
+
+	return person, nil
+}
+
+//GetUserFromDGraph - Returns User
+func GetUserFromDGraph(email string) (*btuser.User, error) {
+	dgraph := DgraphCONN()
+	variables := map[string]string{"$email": email}
+
+	query := `query all($email: string){
+		all(func: q($email)){
+			uid
+			username
+			email
+			accountstatus
+			verificationcode
+			codestatus
+			datejoin
+			lastupdated
+		}
+	}`
+
+	//
+	var usr *btuser.User
+	ctx := context.Background()
+	resp, err := dgraph.NewTxn().QueryWithVars(ctx, query, variables)
+	if err != nil {
+		log.Fatalf("Could not query DGraph. The following error occured: \n %+v \n", err.Error())
+		//return nil, err.Error()
+	}
+
+	//fmt.Println(string(resp.Json))
+	err = json.Unmarshal(resp.Json, &usr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return usr, nil
+}
+
+//IsUserSignedUp - Will check if a user with the given email address is already signed up or not -
+//Param string email
+//return bool true if user is signedup, false if not
+func IsUserSignedUp(email string) bool {
+	userInfo, err := GetUserFromDGraph(email)
+	if err != nil {
+		log.Fatalf("Could not get user information from DGraph the following error occured : \n %+v \n", err)
+	}
+
+	//check string length of returned useruid
+	if len(userInfo.Uuid) > 2 {
+		//user is signed up and has a uuid
+		return true
+	}
+	return false
 }
